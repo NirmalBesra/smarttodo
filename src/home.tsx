8c7cdcc62c35cf0fa1c833ed83299cb4daf95949
Nirmal Besra <kznrml@gmail.com>
2026-02-19 04:40:17 +0530

    improved css now cards look much better

--- src/home.tsx
+++ src/home.tsx
@@ -5,20 +5,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash  } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 const  Home = () => {
-const [cards,setCards] = useState([]);
+type Card = {
+  id: number,
+  taskname:string,
+  priority: string,
+  desc: string
+};
+const [cards,setCards] = useState<Card[]>([]);
 
 useEffect(()=>{
       fetch("http://localhost:8181/getlist")
       .then((res)=>res.json())
       .then((data)=> setCards(data));
 },[]);
+
+const deleteCard = async (id:string) =>{
+      console.log("delete called");
+      try {
+            await fetch('http://localhost:8181/deletetodo', {
+                  method: "DELETE",
+                  headers: 
+                        {
+                              "Content-Type": "application/json"
+                        },
+                  body: JSON.stringify({
+                        "id":`${id}`
+                  })
+                  
+            });
+            setCards(cards.filter(card=> card.id !==id));
+      }
+      catch (error) {
+            console.log(error);
+      }
+};
        return <div className='homeBody'>
                     <h3>Smart Todo</h3>
                     <div className='cardBody'>
                         {
                               cards.map((card)=>(<div key={card.id} className="card">
                                     <div className='taskName'><span className='taskNameSpan'>{card.taskname}</span></div>
-                                    <div className='priority'><span>{card.priority}</span> <span><FontAwesomeIcon icon={faTrash } /></span></div>
+                                    <div className='priority'><span>{card.priority}</span> <span onClick={deleteCard}><FontAwesomeIcon icon={faTrash } /></span></div>
                               </div>))
                         }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import FormUI from './formUI';
+import FormUI from './formUI';
 import Home from './home';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
